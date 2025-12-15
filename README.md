f8a8f0e9f1c960142833e10a53352dcc2b6b9dc2
Aliaksandr Halavach lnv <sasha2015to@gmail.com>
2025-12-15 08:55:39 +0300

    add README.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # cpp-cmake-vscode-example
+
+
+Для построения бинарника:
+
+Ctrl + Shift + B
+
+Запустить (Debug F5)
+
+[Debugging C++ & CMake in VSCode in the Right Way](https://www.youtube.com/watch?v=Qng2RW_bjS8)
--- src/executable/main.cpp
+++ src/executable/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <library/library.hpp>
 
 #include <fmt/format.h>
@@ -7,6 +8,19 @@
 int main()
 {
     std::vector<int> const test{1, 2, 3, 4, 5};
+    for (auto item : test)
+    {
+        std::cout << item << ' ';
+    }
+    std::cout << std::endl;
+    std::cout << "Привет Саша! :)" << std::endl;
+    std::cout << "Ого-го!\n"; 
+
     library::say_hello();
     fmt::println("Hello from the executable");
+
+    std::cout << "Enter number2: \n";
+    std::string number{"14/08/1986"};
+    // std::cin >> number;
+    std::cout << "Our number is: " << number << std::endl;
 }
